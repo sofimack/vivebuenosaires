61c2504c4dacf56c01eb44760d6cb0921180587b
Sofia Mackinnon <sofi.mackinnon@gmail.com>
2020-10-28 16:59:54 -0300

    segundo commit del 28oct

--- jsindex.js
+++ jsindex.js
@@ -1,3 +1,5 @@
+
+
 //ANIMACION - no me toma los milisegundos
 
 $(document).ready(cargarTituloPag);
@@ -15,15 +17,68 @@ const htl = ('Hotel');
 
 
 
+/*
 
+//AJAX
+
+$("#enviar").click(mostrar)
+    
+    function mostrar (){ 
+    $.ajax({
+         url: "objetos.json",    		
+         type: "GET",                    
+         dataType: "json"               
+    }).done(function (resultado) {  	
+               console.log(resultado);
+           })
+      .fail(function (xhr, status, error) {   
+             console.log(xhr); console.log(status); console.log(error);
+            }) 
+  };
+  
 
 var serviciosTraslados = [ezehtl1, ezehtl2, ezehtl3, ezehtl4, ezehtl5, ezehtl6, ezehtlreg, htlezereg, aephtl1, aephtl2, aephtl3, aephtl4, aephtl5, aephtl6, aephtlreg, htlaepreg, ezeaep1, ezeaep2, ezeaep3, ezeaep4, ezeaep5, ezeaep6];
 
 
-var cantidadServicios = serviciosTraslados.length;
+var cantidadServicios = serviciosTraslados.length; */
 
 var tiposDeAutos = ['Corolla 5 puertas', 'H1', 'Van de hasta 10 pax con valijas'];
 
+
+//alert
+
+
+
+$('#enviar').click (chequeo)
+
+
+
+function chequeo () {
+    if ($('#origen').val() == $('#destino').val()) {
+       alert ('El origen y el destino deben ser diferentes')
+};
+
+    
+    
+    if ($('#origen').val() != $('#destino').val()) {
+        $('#enviar').click (popup);
+        $('.popup-close').click (cerrarPopUp);
+
+        var pop = document.querySelector('.popup-wrapper');
+        
+        function popup () {
+        pop.style.display = 'block';
+        }
+        function cerrarPopUp () {
+        pop.style.display = 'none';
+
+        
+        
+        };
+
+    }
+
+} 
 
 
 
@@ -58,18 +113,18 @@ function Datos(desde, hasta, cantPax, seleccion) {
 
 function guardarEnLocal() {
 
-    var desde = document.getElementById('origen').value;
-    var hasta = document.getElementById('destino').value;
-    var cantPax = document.getElementById('pasajeros').value;
+    var desde = $('#origen').val();
+    var hasta = $('#destino').val();
+    var cantPax = $('#pasajeros').val();
     var datos = new Datos (desde, hasta, cantPax)
-    var privado = document.getElementById('gridRadios1');
-    console.log(privado)
+    var privado = $('#gridRadios1');
+    //console.log(privado)
      if (privado.checked == true) {
        datos.seleccion = 'Privado'
       
     }
 
-    var regular = document.getElementById('gridRadios2');
+    var regular = $('#gridRadios2');
     if (regular.checked == true){
         datos.seleccion = 'Regular'
        
@@ -77,11 +132,5 @@ function guardarEnLocal() {
     localStorage.setItem ('datos ingresados', JSON.stringify(datos))
 }
 
-obtenerDatosJson();
 
-function obtenerDatosJson () {
-    var obtener = localStorage.getItem ('datos ingresados');
-    console.log (JSON.parse(obtener));
-    console.log (obtener.seleccion);
-}
 
